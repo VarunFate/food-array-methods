7a5fd5c758d55460cebc0faf0b8dd9bc8e757449
VarunFate <75355086+VarunFate@users.noreply.github.com>
2023-08-06 18:18:52 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+You have been provided with a food chart food.json file.
+
+[food.json](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/a438c663-645c-4570-8503-11f20cea9b18/food.json)
+
+You task is to apply different arrays methods to get the desired following result.
+
+- [ ]  list all the food items
+- [ ]  list all the food items with category vegetables
+- [ ]  list all the food items with category fruit
+- [ ]  list all the food items with category protien
+- [ ]  list all the food items with category nuts
+- [ ]  list all the food items with category grains
+- [ ]  list all the food items with category dairy
+- [ ]  list all the food items with calorie above 100
+- [ ]  list all the food items with calorie below 100
+- [ ]  list all the food items with highest protien content to lowest
+- [ ]  list all the food items with lowest cab content to highest
+
+Note: You will need to create functions for different operations and you will pass the original data as the input to the function and the function should return the desired result
---
+++ foodChart.js
@@ -0,0 +1,501 @@
+// Create an array of JSON objects
+const foodArray = [
+  {
+    "id": 1,
+    "foodname": "Apple",
+    "calorie": 95,
+    "category": "Fruit",
+    "protiens": 0.3,
+    "cab": 25
+  },
+  {
+    "id": 2,
+    "foodname": "Banana",
+    "calorie": 105,
+    "category": "Fruit",
+    "protiens": 1.3,
+    "cab": 27
+  },
+  {
+    "id": 3,
+    "foodname": "Orange",
+    "calorie": 62,
+    "category": "Fruit",
+    "protiens": 1.2,
+    "cab": 15
+  },
+  {
+    "id": 4,
+    "foodname": "Pear",
+    "calorie": 95,
+    "category": "Fruit",
+    "protiens": 0.5,
+    "cab": 25
+  },
+  {
+    "id": 5,
+    "foodname": "Grapefruit",
+    "calorie": 42,
+    "category": "Fruit",
+    "protiens": 1.1,
+    "cab": 11
+  },
+  {
+    "id": 6,
+    "foodname": "Strawberry",
+    "calorie": 46,
+    "category": "Fruit",
+    "protiens": 1.5,
+    "cab": 10
+  },
+  {
+    "id": 7,
+    "foodname": "Blueberry",
+    "calorie": 52,
+    "category": "Fruit",
+    "protiens": 1.1,
+    "cab": 14
+  },
+  {
+    "id": 8,
+    "foodname": "Raspberry",
+    "calorie": 52,
+    "category": "Fruit",
+    "protiens": 1.4,
+    "cab": 15
+  },
+  {
+    "id": 9,
+    "foodname": "Broccoli",
+    "calorie": 34,
+    "category": "Vegetable",
+    "protiens": 3.3,
+    "cab": 5
+  },
+  {
+    "id": 10,
+    "foodname": "Cauliflower",
+    "calorie": 25,
+    "category": "Vegetable",
+    "protiens": 2.6,
+    "cab": 5
+  },
+  {
+    "id": 11,
+    "foodname": "Green Beans",
+    "calorie": 31,
+    "category": "Vegetable",
+    "protiens": 2.4,
+    "cab": 4
+  },
+  {
+    "id": 12,
+    "foodname": "Asparagus",
+    "calorie": 20,
+    "category": "Vegetable",
+    "protiens": 2.2,
+    "cab": 2
+  },
+  {
+    "id": 13,
+    "foodname": "Spinach",
+    "calorie": 23,
+    "category": "Vegetable",
+    "protiens": 3.5,
+    "cab": 4
+  },
+  {
+    "id": 14,
+    "foodname": "Kale",
+    "calorie": 33,
+    "category": "Vegetable",
+    "protiens": 4.3,
+    "cab": 7
+  },
+  {
+    "id": 15,
+    "foodname": "Sweet Potato",
+    "calorie": 103,
+    "category": "Vegetable",
+    "protiens": 2,
+    "cab": 27
+  },
+  {
+    "id": 16,
+    "foodname": "Potato",
+    "calorie": 161,
+    "category": "Vegetable",
+    "protiens": 4.3,
+    "cab": 37
+  },
+  {"id": 17,
+    "foodname": "Carrot",
+    "calorie": 41,
+    "category": "Vegetable",
+    "protiens": 0.9,
+    "cab": 9
+  },
+  {
+    "id": 18,
+    "foodname": "Onion",
+    "calorie": 40,
+    "category": "Vegetable",
+    "protiens": 1.4,
+    "cab": 9
+  },
+  {
+    "id": 19,
+    "foodname": "Egg",
+    "calorie": 77,
+    "category": "Protein",
+    "protiens": 6.3,
+    "cab": 0.5
+  },
+  {
+    "id": 20,
+    "foodname": "Chicken Breast",
+    "calorie": 165,
+    "category": "Protein",
+    "protiens": 31,
+    "cab": 0
+  },
+  {
+    "id": 21,
+    "foodname": "Salmon",
+    "calorie": 206,
+    "category": "Protein",
+    "protiens": 22,
+    "cab": 0
+  },
+  {
+    "id": 22,
+    "foodname": "Tuna",
+    "calorie": 179,
+    "category": "Protein",
+    "protiens": 39,
+    "cab": 0
+  },
+  {
+    "id": 23,
+    "foodname": "Beef",
+    "calorie": 250,
+    "category": "Protein",
+    "protiens": 26,
+    "cab": 0
+  },
+  {
+    "id": 24,
+    "foodname": "Pork",
+    "calorie": 242,
+    "category": "Protein",
+    "protiens": 26,
+    "cab": 0
+  },
+  {
+    "id": 25,
+    "foodname": "Lamb",
+    "calorie": 294,
+    "category": "Protein",
+    "protiens": 25,
+    "cab": 0
+  },
+  {
+    "id": 26,
+    "foodname": "Shrimp",
+    "calorie": 85,
+    "category": "Protein",
+    "protiens": 20,
+    "cab": 0.2
+  },
+  {
+    "id": 27,
+    "foodname": "Quinoa",
+    "calorie": 120,
+    "category": "Grain",
+    "protiens": 4.4,
+    "cab": 21
+  },
+  {
+    "id": 28,
+    "foodname": "Brown Rice",
+    "calorie": 216,
+    "category": "Grain",
+    "protiens": 4.5,
+    "cab": 45
+  },
+  {
+    "id": 29,
+    "foodname": "Oats",
+    "calorie": 389,
+    "category": "Grain",
+    "protiens": 16.9,
+    "cab": 66
+  },
+  {
+    "id": 30,
+    "foodname": "Quinoa",
+    "calorie": 120,
+    "category": "Grain",
+    "protiens": 4.4,
+    "cab": 21
+  },
+  {
+    "id": 31,
+    "foodname": "Bread",
+    "calorie": 265,
+    "category": "Grain",
+    "protiens": 9.4,
+    "cab": 49
+  },
+  {
+    "id": 32,
+    "foodname": "Pasta",
+    "calorie": 131,
+    "category": "Grain",
+    "protiens": 5.5,
+    "cab": 26
+  },
+  {
+    "id": 33,
+    "foodname": "Milk",
+    "calorie": 60,
+    "category": "Dairy",
+    "protiens": 3.2,
+    "cab": 5.1
+  },
+  {
+    "id": 34,
+    "foodname": "Cheese",
+    "calorie": 402,
+    "category": "Dairy",
+    "protiens": 25,
+    "cab": 2.4
+  },
+  {
+    "id": 35,
+    "foodname": "Yogurt",
+    "calorie": 59,
+    "category": "Dairy",
+    "protiens": 3.5,
+    "cab": 5
+  },
+  {
+    "id": 36,
+    "foodname": "Butter",
+    "calorie": 717,
+    "category": "Dairy",
+    "protiens": 0.9,
+    "cab": 0.1
+  },
+  {
+    "id": 37,
+    "foodname": "Almonds",
+    "calorie": 579,
+    "category": "Nuts",
+    "protiens": 21,
+    "cab": 22
+  },
+  {
+    "id": 38,
+    "foodname": "Walnuts",
+    "calorie": 654,
+    "category": "Nuts",
+    "protiens": 15,
+    "cab": 14
+  },
+  {
+    "id": 39,
+    "foodname": "Peanuts",
+    "calorie": 567,
+    "category": "Nuts",
+    "protiens": 26,
+    "cab": 16
+  },
+  {
+    "id": 40,
+    "foodname": "Cashews",
+    "calorie": 553,
+    "category": "Nuts",
+    "protiens": 18,
+    "cab": 30
+  }
+  // Add more JSON objects as needed
+];
+// **************************************************************
+// Function to list all the food items
+function listFoodItems(foodArray) {
+  // Iterate over the array of JSON objects
+  for (const foodItem of foodArray) {
+    // Extract the 'foodName' property from each JSON object
+    const foodname = foodItem.foodname;
+
+    // Output the food item's name
+    console.log(foodname);
+  }
+}
+listFoodItems(foodArray);
+
+// **************************************************************
+
+// Function to list all the food items with category "Vegetable"
+function listVegetables(foodArray) {
+  // Iterate over the array of JSON objects
+  for (const foodItem of foodArray) {
+    // Check if the category is "Vegetable"
+    if (foodItem.category === 'Vegetable') {
+      // Extract the 'foodname' property from the JSON object
+      const foodName = foodItem.foodname;
+
+      // Output the food item's name
+      console.log(foodName);
+    }
+  }
+}
+// Call the function with the array 'foodArray' to list vegetables
+listVegetables(foodArray);
+
+// **************************************************************
+
+// Function to list all food items with category "fruits"
+function listFruits(foodArray) {
+  // iterate over the array of JSON objects
+  for (const foodItem of foodArray) {
+    // check if the category is "fruits"
+    if (foodItem.category === 'Fruit') {
+      // extract the 'foodname' property from the json object
+      const foodName = foodItem.foodname;
+
+      // output the item's name
+      console.log(foodName);
+    }
+  }
+}
+// call the function with the array 'foodArray' to list all fruits
+listFruits(foodArray);
+
+// **************************************************************
+
+// function to list all food items with category "Protein"
+function listProtein(foodArray) {
+  for (const foodItem of foodArray) {
+    if (foodItem.category === "Protein") {
+      const foodName = foodItem.foodname;
+
+      // output
+      console.log(foodName);
+    }
+  }
+}
+// call
+listProtein(foodArray);
+
+// **************************************************************
+// function to list all food items with category "nuts"
+function listProtein(foodArray) {
+  for (const foodItem of foodArray) {
+    if (foodItem.category === "Nuts") {
+      const foodName = foodItem.foodname;
+
+      // output
+      console.log(foodName);
+    }
+  }
+}
+// call function to list all food items with categoty "Nuts"
+listProtein(foodArray);
+
+// **************************************************************
+// function to list all food items with category "Grain"
+function listProtein(foodArray) {
+  for (const foodItem of foodArray) {
+    if (foodItem.category === "Grain") {
+      const foodName = foodItem.foodname;
+
+      // output
+      console.log(foodName);
+    }
+  }
+}
+// call function to list all food items with categoty "Grain"
+listProtein(foodArray);
+
+// **************************************************************
+// function to list all food items with category "Dairy"
+function listProtein(foodArray) {
+  for (const foodItem of foodArray) {
+    if (foodItem.category === "Dairy") {
+      const foodName = foodItem.foodname;
+
+      // output
+      console.log(foodName);
+    }
+  }
+}
+// call function to list all food items with categoty "Dairy"
+listProtein(foodArray);
+
+// **************************************************************
+// function to list all food items with calorie above 100
+function listProtein(foodArray) {
+  for (const foodItem of foodArray) {
+    if (foodItem.calorie > 100) {
+      const foodName = foodItem.foodname;
+
+      // output
+      console.log(foodName);
+    }
+  }
+}
+// call function to list all food items with calorie above 100
+listProtein(foodArray);
+
+// **************************************************************
+// function to list all food items with calorie below 100
+function listProtein(foodArray) {
+  for (const foodItem of foodArray) {
+    if (foodItem.calorie < 100) {
+      const foodName = foodItem.foodname;
+
+      // output
+      console.log(foodName);
+    }
+  }
+}
+// call function to list all food items with calorie below 100
+listProtein(foodArray);
+
+// **************************************************************
+// Function to list all the food items with highest protein content to lowest
+function listFoodItemsByProteinContent(foodArray) {
+  // Sort the array based on the "proteins" property in descending order
+  const sortedFoodArray = foodArray.sort((a, b) => b.proteins - a.proteins);
+
+  // Iterate over the sorted array and output the food item names
+  for (const foodItem of sortedFoodArray) {
+    const foodName = foodItem.foodname;
+    console.log(foodName);
+  }
+}
+
+// Call the function with the array 'foodArray' to list food items by protein content
+listFoodItemsByProteinContent(foodArray);
+
+// **************************************************************
+// Function to list all the food items with lowest cab content to highest
+function listFoodItemsByCabContent(foodArray) {
+  // Sort the array based on the "cab" property in ascending order
+  const sortedFoodArray = foodArray.sort((a, b) => a.cab - b.cab);
+
+  // Iterate over the sorted array and output the food item names
+  for (const foodItem of sortedFoodArray) {
+    const foodName = foodItem.foodname;
+    console.log(foodName);
+  }
+}
+
+// Call the function with the array 'foodArray' to list food items by cab content
+listFoodItemsByCabContent(foodArray);
+
+
+// **************************************************************
+
